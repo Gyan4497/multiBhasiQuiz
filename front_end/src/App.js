f7ab23dd6acacd1d2ccee00eafae497ceb2a1826
Gyan <gyanprakash92@gmail.com>
2017-09-06 03:33:59 +0530

    showing Question

--- front_end/src/App.js
+++ front_end/src/App.js
@@ -1,21 +1,55 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import ReactDom from 'react-dom';
+import axios from 'axios';
 
 class App extends Component {
+
+  constructor() {
+    super();
+    this.state = {
+      data: [],
+      status: 'loading'
+    };
+  }
+
+  componentDidMount() {
+    axios.get('http://localhost:8080/question')
+    .then(res => {
+      const data = res.data.children.map(obj => obj.data);
+      this.setState({ data });
+    });
+  }
+
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      // <div className="App">
+      //   <div className="App-header">
+      //     //<img src={logo} className="App-logo" alt="logo" />
+      //     <h2>Welcome to React</h2>
+      // //   </div>
+      // </div>
+
+      <div>
+        <ul>
+          {this.state.data.map(data =>
+            <li key={data.id}>{data.ques}</li>
+            // <li>{data.option1}</li>
+            // <li>{data.option2}</li>
+            // <li>{data.option3}</li>
+            // <li>{data.option4}</li>
+          )}
+        </ul>
+        <button> Next </button>
       </div>
     );
   }
 }
+
+// class Question extends Component {
+//   render() {
+//     return();
+//   }
+// }
 
 export default App;
