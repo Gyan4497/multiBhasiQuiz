2a8e0389cfadc227c222ac04d3c369cbffeb454c
Gyan4497 <gyanprakash92@gmail.com>
2018-04-21 11:07:15 +0530

    changes for demo gyan

--- front_end/src/App.js
+++ front_end/src/App.js
@@ -1,7 +1,7 @@
-import React, { Component } from 'react';
-import './App.css';
-import ReactDom from 'react-dom';
-import axios from 'axios';
+import React, { Component } from "react";
+import "./App.css";
+import ReactDom from "react-dom";
+import axios from "axios";
 
 class App extends Component {
   constructor() {
@@ -10,63 +10,84 @@ class App extends Component {
       data: [],
       count: 0,
       quesObj: [],
-      status: 'loading'
+      status: "loading"
     };
   }
 
   componentDidMount() {
-    axios.get('http://localhost:8080/question')
-    .then(res => {
+    axios.get("http://localhost:8080/question").then(res => {
       console.log(res);
       const data = res.data.map(obj => obj);
       this.setState({ data });
 
-      console.log(data);
-
+      console.log(data + "gyan");
     });
   }
 
-  onclick(type){
-    this.setState({count: this.state.count+1});
+  onclick(type) {
+    this.setState({ count: this.state.count + 1 });
 
     const quesObj = this.state.data[this.state.count];
-    if(quesObj == null) {
+    if (quesObj == null) {
       alert("Bye !!!");
-    }
-    else 
-    this.setState({ quesObj });
-    
+    } else this.setState({ quesObj });
   }
 
   render() {
     return (
       <div class="quizBody">
-        <Question quesObj={this.state.quesObj}/>
-        
-        {this.state.count==0 ?
-          <input type='button' onClick={this.onclick.bind(this, 'start')} value='StartTest' className="start"/>
-          :<input type='button' onClick={this.onclick.bind(this, 'next')} value='Next' className="next"/>
-        }
+        <Question quesObj={this.state.quesObj} />
+
+        {this.state.count == 0 ? (
+          <input
+            type="button"
+            onClick={this.onclick.bind(this, "start")}
+            value="StartTest"
+            className="start"
+          />
+        ) : (
+          <input
+            type="button"
+            onClick={this.onclick.bind(this, "next")}
+            value="Next"
+            className="next"
+          />
+        )}
       </div>
     );
   }
 }
 
 class Question extends Component {
-  optionsCheck = (options) => {
+  optionsCheck = options => {
     console.log(options);
     console.log(this.props.quesObj.ans);
-    if(options === this.props.quesObj.ans) 
-    {
-      alert("Correct answer!!!!")
+    if (options === this.props.quesObj.ans) {
+      alert("Correct answer!!!!");
     }
-  }
+  };
 
   render() {
-    return(
+    return (
       <div>
-        <div key={this.props.quesObj.id} className="question"> {this.props.quesObj.ques}</div>
-        <div>{(this.props.quesObj.options!= undefined) ? this.props.quesObj.options.map(options => (<input type='button' onClick={() => {this.optionsCheck(options)}} value={options} className="options"/>)):null }</div>
+        <div key={this.props.quesObj.id} className="question">
+          {" "}
+          {this.props.quesObj.ques}
+        </div>
+        <div>
+          {this.props.quesObj.options != undefined
+            ? this.props.quesObj.options.map(options => (
+                <input
+                  type="button"
+                  onClick={() => {
+                    this.optionsCheck(options);
+                  }}
+                  value={options}
+                  className="options"
+                />
+              ))
+            : null}
+        </div>
       </div>
     );
   }
